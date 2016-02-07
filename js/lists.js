ea4b7f38f108a4f4bc9251a60672c61848cbb879
UltimateDesignSlayer <farhadinho99@googlemail.com>
2016-02-07 18:08:49 +0000

    Move items between todo and done lists

--- js/lists.js
+++ js/lists.js
@@ -12,16 +12,33 @@ lists = {
 	
 		that.listTitle = "List";
 		that.newItem = ko.observable();
+		that.newItemDesc = ko.observable();
 		
 		that.toDoList = ko.observableArray();
 		that.doneList = ko.observableArray();
 		
 		that.addListItems = function(){
-			that.toDoList.push({name:that.newItem()});
+			that.toDoList.push({name:that.newItem(), desc:that.newItemDesc()});
+			localStorage.setItem('toDoList', that.toDoList());
+			localStorage.setItem('doneList', that.doneList());
 		}
 		
 		that.removeListItems = function(list){
 			that.toDoList.remove(this);
+			that.doneList.remove(this);
+			//Update Local storage
+			localStorage.setItem('toDoList', JSON.Stringify(that.toDoList()));
+			localStorage.setItem('doneList', that.doneList());
+		}
+		
+		that.markAsDone = function(){
+			that.doneList.push(this);
+			that.toDoList.remove(this);
+		}
+		
+		that.markAsNotDone = function(){
+			that.toDoList.push(this);
+			that.doneList.remove(this);
 		}
 	},
 	eventBindings:function(){
